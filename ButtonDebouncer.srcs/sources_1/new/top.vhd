067006b1f4addcb90324ff142e296672da7112a2
Amadeusz Zabierowski <zabierowskiamadeusz@gmail.com>
2023-03-13 22:36:31 +0100

    Project init

---
+++ ButtonDebouncer.srcs/sources_1/new/top.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company:
+-- Engineer:
+--
+-- Create Date: 03/13/2023 10:29:47 PM
+-- Design Name:
+-- Module Name: top - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top is
+    Port ( CLK : in STD_LOGIC;
+           RESET : in STD_LOGIC;
+           input : in STD_LOGIC;
+           output : out STD_LOGIC);
+end top;
+
+architecture Behavioral of top is
+
+begin
+
+
+end Behavioral;
