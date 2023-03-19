c096310ad2510f96840afed2e028abce31d8ff02
Amadeusz Zabierowski <zabierowskiamadeusz@gmail.com>
2023-03-19 20:05:43 +0100

    top.vhd finished

--- ButtonDebouncer.srcs/sources_1/new/top.vhd
+++ ButtonDebouncer.srcs/sources_1/new/top.vhd
@@ -1,46 +1,97 @@
-----------------------------------------------------------------------------------
--- Company:
--- Engineer:
---
--- Create Date: 03/13/2023 10:29:47 PM
--- Design Name:
--- Module Name: top - Behavioral
--- Project Name:
--- Target Devices:
--- Tool Versions:
--- Description:
---
--- Dependencies:
---
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.std_logic_unsigned.ALL;
 
 entity top is
-    Port ( CLK : in STD_LOGIC;
-           RESET : in STD_LOGIC;
-           input : in STD_LOGIC;
-           output : out STD_LOGIC);
-end top;
+    Port ( button : in STD_LOGIC;
+           led : out STD_LOGIC;
+           CLK : in STD_LOGIC;
+           RESET : in STD_LOGIC
+           );
+end entity top;
 
-architecture Behavioral of top is
+architecture top_arch of top is
+    --deklaracja stan?w
+    type state is( stabilny, opoznienie, niestabilny);
+    --stan pocz?tkowy przyjmujemy jako stabilny
+    signal current_state : state := stabilny;
+    signal next_state : state := stabilny;
+
+    --rejestr licznika dla op?znienia
+    signal counter_op : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
+    signal counter_niestb : STD_LOGIC_VECTOR(10 downto 0 ) := (others => '0');
+
 
 begin
+    --proces przejscia stan?w
+    state_change: process(CLK) is
+    begin
+        if(RESET  = '0') then
+            current_state <= stabilny ;
+        elsif(CLK'event and CLK = '1') then
+            current_state <= next_state;
+        end if;
+    end process state_change;
+
+    -- proces licznika opoznienia
+    counter_op_prc: process(CLK, RESET,current_state, button) is
+        begin
+        if(RESET = '0') then
+            counter_op <= (others => '0');
+        elsif(CLK'event and CLK = '1') then
+            if(current_state = opoznienie ) then
+                   counter_op <= counter_op  + 1;
+                   if(next_state  /= opoznienie ) then
+                        counter_op  <= (others  => '0');
+                   end if;
+            else
+                counter_op  <= (others => '0');
+            end if;
+        end if;
+    end process counter_op_prc;
+
+     -- proces licznika niestabilnego
+    counter_prc: process(CLK, RESET, current_state ) is
+        begin
+         if(RESET = '0') then
+            counter_niestb <= (others => '0');
+             -- reset licznika do doliczeniu 1000 ms
+         elsif(CLK'event and CLK = '1') then
+                if(current_state = niestabilny) then
+                    counter_niestb <= counter_niestb + 1;
+                else
+                    counter_niestb <=(others =>'0');
+                end if;
+         end if;
+    end process counter_prc;
 
 
-end Behavioral;
+
+    --opis przej?? mi?dzy stanami
+    state_conditions: process(button, counter_niestb, counter_op,next_state, current_state) is
+    begin
+    next_state <= current_state;
+        case current_state is
+            when stabilny =>
+                if(button = '1') then
+                    next_state <= opoznienie ;
+                end if;
+            when opoznienie =>
+                if (counter_op = "1111") then
+                    if(button = '1') then
+                        next_state <= niestabilny;
+                    else
+                        next_state <=stabilny;
+                    end if;
+                end if;
+           when niestabilny =>
+                if(counter_niestb = "11111111111")then
+                    next_state <= stabilny ;
+                else
+                    next_state <= niestabilny ;
+                 end if;
+        end case;
+    end process state_conditions;
+    --stan wyjscia
+    led <= '1' when current_state  = niestabilny else '0';
+end architecture top_arch;
